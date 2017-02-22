1e13f35e9ec2e7e3c090d7df7fd18e5bf1f839fa
Jarno <ruedigerbusche@web.de>
2017-02-22 22:29:44 +0100

    Remove lecture class

--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -1,6 +1,5 @@
 from scheduler.instructor import Instructor
 from scheduler.timeslot import Timeslot
-from scheduler.lecture import Lecture
 from scheduler.room import Room
 from scheduler.timetable import Timetable
 from scheduler.assignment import Assignment
